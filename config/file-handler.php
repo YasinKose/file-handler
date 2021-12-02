13b404b39517181e6d585aa4907210475c1892e8
Yasin Köse <yasin.kose.123@gmail.com>
2021-12-02 16:04:54 +0300

    Add table-name config for migrations

--- config/file-handler.php
+++ config/file-handler.php
@@ -2,5 +2,7 @@
 
 return [
     'server-url' => env("FILE_HANDLER_URL"),
-    'api-key' => env("FILE_HANDLER_API_KEY")
+    'api-key' => env("FILE_HANDLER_API_KEY"),
+
+    'table-name' => env("FILE_HANDLER_TABLE_NAME", "files")
 ];
