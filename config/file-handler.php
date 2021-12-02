992847ca4dda01aa6237c6f07c463bf8e97732d1
Yasin Köse <yasin.kose.123@gmail.com>
2021-12-02 18:07:45 +0300

    Add model config in file-handler.php

--- config/file-handler.php
+++ config/file-handler.php
@@ -4,5 +4,6 @@ return [
     'server-url' => env("FILE_HANDLER_URL"),
     'api-key' => env("FILE_HANDLER_API_KEY"),
 
-    'table-name' => env("FILE_HANDLER_TABLE_NAME", "files")
+    'table-name' => env("FILE_HANDLER_TABLE_NAME", "files"),
+    'model' => YasinKose\FileHandler\Models\File::class
 ];
